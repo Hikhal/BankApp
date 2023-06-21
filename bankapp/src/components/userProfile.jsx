d302240c195eb3ecb3774d0c3ba92fa7ef70d528
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-20 22:03:52 -0400

    debit link uploaded

--- bankapp/src/components/userProfile.jsx
+++ bankapp/src/components/userProfile.jsx
@@ -3,9 +3,12 @@ export default function UserProfile(props) {
     
   return (
     <div>
-      <h1>User Profile</h1>
-      <h3>Name: Hamza Khaliq</h3>
-      <h3>balance: {props.bal1}</h3>
+      <h3>First Name: Hamza </h3>
+      <h3>Last Name: Khaliq </h3>
+      <h3>Account Number: 123456789 </h3>
+      <h3>Routing Number: 073-1237401 </h3>
+      <h3>Contact: yadayadayada@getalife.com</h3>
+      <h3>Account Balance: {props.bal1}</h3>
     </div>
   );
 }
